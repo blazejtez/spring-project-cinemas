ce9e810ff3e3df7b8661f001c9aaa702a7f13e89
blazejtez <79143501+blazejtez@users.noreply.github.com>
2023-10-30 11:36:08 +0100

    implementation of entity classes with comparison and text repr

---
+++ src/main/java/pl/edu/pg/menu/caffemenu/DTO/DishDTO.java
@@ -0,0 +1,2 @@
+package pl.edu.pg.menu.caffemenu.DTO;public class DishDTO {
+}
--- src/main/java/pl/edu/pg/menu/caffemenu/entity/Dish.java
+++ src/main/java/pl/edu/pg/menu/caffemenu/entity/Dish.java
@@ -3,13 +3,15 @@ package pl.edu.pg.menu.caffemenu.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @Entity
-public class Dish {
+@EqualsAndHashCode
+public class Dish implements Comparable<Dish>{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,5 +23,15 @@ public class Dish {
     @ManyToOne
     private Menu menu;
 
+
+    @Override
+    public int compareTo(Dish o) {
+        return this.price.compareTo(o.getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Dish [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
 
 }
