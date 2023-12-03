2bc7b2208e42ed38f6c0bb1b5ce923c8b27f6d2c
Blazej Tez <blatez@student.pg.edu.pl>
2023-12-03 18:54:27 +0100

    added CommandLineRunner file

---
+++ src/main/java/pl/edu/pg/menu/caffemenu/util/CommandLine.java
@@ -0,0 +1,8 @@
+package pl.edu.pg.menu.caffemenu.util;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLine implements CommandLineRunner {
+}
