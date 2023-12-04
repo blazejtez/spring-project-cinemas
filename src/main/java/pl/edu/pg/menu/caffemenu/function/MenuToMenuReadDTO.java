070984c13772c668b4fec8fcb6eba121dee93a38
Blazej Tez <blatez@student.pg.edu.pl>
2023-12-04 06:17:51 +0100

    only updateDTOs left to do

--- src/main/java/pl/edu/pg/menu/caffemenu/function/MenuToMenuReadDTO.java
+++ src/main/java/pl/edu/pg/menu/caffemenu/function/MenuToMenuReadDTO.java
@@ -1,4 +1,18 @@
 package pl.edu.pg.menu.caffemenu.function;
 
-public class MenuToMenuReadDTO {
+import pl.edu.pg.menu.caffemenu.dto.MenuReadDTO;
+import pl.edu.pg.menu.caffemenu.entity.Menu;
+
+import java.util.function.Function;
+
+public class MenuToMenuReadDTO implements Function<Menu, MenuReadDTO> {
+
+    @Override
+    public MenuReadDTO apply(Menu entity) {
+        return MenuReadDTO.builder()
+                .name(entity.getName())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .build();
+    }
 }
