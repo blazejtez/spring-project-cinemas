6d4f14b71171e18c56da661aade8138ef9a10cea
Blazej Tez <blatez@student.pg.edu.pl>
2023-12-04 05:57:57 +0100

    added implementation of transform from Menus to MenusReadDTO

--- src/main/java/pl/edu/pg/menu/caffemenu/function/MenuToMenusReadDTO.java
+++ src/main/java/pl/edu/pg/menu/caffemenu/function/MenuToMenusReadDTO.java
@@ -1,4 +1,21 @@
 package pl.edu.pg.menu.caffemenu.function;
 
-public class MenuToMenusReadDTO {
+import pl.edu.pg.menu.caffemenu.dto.MenusReadDTO;
+import pl.edu.pg.menu.caffemenu.entity.Menu;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class MenuToMenusReadDTO implements Function<List<Menu>, MenusReadDTO> {
+    @Override
+    public MenusReadDTO apply(List<Menu> entities) {
+        return MenusReadDTO.builder()
+                .menus(entities.stream()
+                        .map( menu -> MenusReadDTO.Menu.builder()
+                                .id(menu.getId())
+                                .name(menu.getName())
+                                .build())
+                        .toList())
+                .build();
+    }
 }
