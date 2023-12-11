322fcd124e99a593de2f3bc0ef84b903c5d8a071
Blazej Tez <blatez@student.pg.edu.pl>
2023-12-11 13:33:52 +0100

    Mapping function for Cinemas done

--- src/main/java/pl/edu/pg/menu/cinemashowtimes/function/CinemasToCinemasReadDTO.java
+++ src/main/java/pl/edu/pg/menu/cinemashowtimes/function/CinemasToCinemasReadDTO.java
@@ -7,14 +7,14 @@ import pl.edu.pg.menu.cinemashowtimes.entity.Cinema;
 import java.util.List;
 import java.util.function.Function;
 @Component
-public class MenuToMenusReadDTO implements Function<List<Cinema>, CinemasReadDTO> {
+public class CinemasToCinemasReadDTO implements Function<List<Cinema>, CinemasReadDTO> {
     @Override
     public CinemasReadDTO apply(List<Cinema> entities) {
         return CinemasReadDTO.builder()
                 .cinemas(entities.stream()
-                        .map( menu -> CinemasReadDTO.Cinema.builder()
-                                .id(menu.getId())
-                                .name(menu.getName())
+                        .map( cinema -> CinemasReadDTO.Cinema.builder()
+                                .id(cinema.getId())
+                                .description(cinema.getDescription())
                                 .build())
                         .toList())
                 .build();
