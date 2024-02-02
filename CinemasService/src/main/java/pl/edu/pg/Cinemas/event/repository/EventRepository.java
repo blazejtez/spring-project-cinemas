b30d2c86fd7448195fdcb91ae8aa4326945b095a
Blazej Tez <blatez@student.pg.edu.pl>
2024-02-02 14:07:29 +0100

    added load balancer for service communication

--- CinemasService/src/main/java/pl/edu/pg/Cinemas/event/repository/EventRepository.java
+++ CinemasService/src/main/java/pl/edu/pg/Cinemas/event/repository/EventRepository.java
@@ -2,31 +2,39 @@ package pl.edu.pg.Cinemas.event.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 import pl.edu.pg.Cinemas.dto.CinemaCreateDTO;
 import pl.edu.pg.Cinemas.entity.Cinema;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Repository
 public class EventRepository {
 
-
+    private LoadBalancerClient loadBalancerClient;
     private final RestTemplate restTemplate;
 
+//    @Value("${isa.showtime.url}")
+//    private String baseUrl;
+    @Value("${eureka.client.serviceurl.defaultzone}")
+    private String eurekaUrl;
     @Value("${isa.showtime.url}")
     private String baseUrl;
 
     @Autowired
-    public EventRepository(RestTemplate restTemplate) {
+    public EventRepository(RestTemplate restTemplate, LoadBalancerClient loadBalancerClient) {
         this.restTemplate = restTemplate;
+        this.loadBalancerClient = loadBalancerClient;
     }
 
     public void delete(UUID uuid) {
+        URI uri = loadBalancerClient.choose("lb://cinemas").getUri();
         String uuidASString = uuid.toString();
-        restTemplate.delete(baseUrl + "/api/cinemas/" + uuidASString);
+        restTemplate.delete(uri + "/api/cinemas/" + uuidASString);
     }
 
     public void create(Cinema cinema) {
